8cd4e96f9f61173ed6695beb192db31b40a2d035
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2022-09-11 11:05:02 -0500

    create in partidos

--- Torneo.App.Frontend/Pages/Partidos/Create.cshtml.cs
+++ Torneo.App.Frontend/Pages/Partidos/Create.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Torneo.App.Dominio;
+using Torneo.App.Persistencia;
 
 namespace Torneo.App.Frontend.Pages.Partidos
 {
     public class CreateModel : PageModel
     {
+        private readonly IRepositorioEquipo _repoEquipo;
+        private readonly IRepositorioPartido _repoPartido;
+
+        public Partido partido { get; set; }
+
+        public IEnumerable<Equipo> equipos { get; set; }
+
+        public CreateModel(IRepositorioPartido repoPartido, IRepositorioEquipo
+repoEquipo)
+        {
+            _repoEquipo = repoEquipo;
+            _repoPartido = repoPartido;
+        }
+
         public void OnGet()
         {
+            partido = new Partido();
+            equipos = _repoEquipo.GetAllEquipos();
         }
+
+    public IActionResult OnPost(Partido partido, int idLocal, int idVisitante)
+    {
+        _repoPartido.AddPartido(partido, idLocal, idVisitante);
+        return RedirectToPage("Index");
+    }
+
     }
 }
