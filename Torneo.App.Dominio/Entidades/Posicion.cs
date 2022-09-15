c3778803fe5e054a5fccf5d4bc8c415cf905349a
Rene Avila Alonso <rene.avila.alonso@gmail.com>
2022-09-14 20:25:57 -0500

    edit en posiciones

--- Torneo.App.Dominio/Entidades/Posicion.cs
+++ Torneo.App.Dominio/Entidades/Posicion.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 namespace Torneo.App.Dominio
 {
     public class Posicion
     {
         public int Id { get; set; }
+        [Display(Name = "Nombre posicion")]
+        [Required(ErrorMessage = "El nombre de la posicion jugador es Obligatoria")]
         public string Nombre { get; set; }
     }
 }
