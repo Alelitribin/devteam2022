fd78a0f21fea3e7ff4f800d1940e7f3259587a17
Alejandra Tribin <tribinpalacios@gmail.com>
2022-09-17 16:56:20 -0500

    validaciones dt, posicion, municipio y arreglo menu

--- Torneo.App.Dominio/Entidades/DirectorTecnico.cs
+++ Torneo.App.Dominio/Entidades/DirectorTecnico.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Torneo.App.Dominio
 {
     public class DirectorTecnico
     {
         public int Id { get; set; }
+        [Display(Name = "Nombre Director Tecnico")]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Nombre { get; set; }
+        [Display(Name = "Numero del Documento")]
+        [Required(ErrorMessage = "El Documento es obligatorio")]
         public string Documento { get; set; }
+        [Display(Name = "Numero de Telefono")]
+        [Required(ErrorMessage = "El Telefono es obligatorio")]
         public string Telefono { get; set; }
     }
 }
